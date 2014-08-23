a8838e90f7a5fe1a202cf07643b868c6df779433
alexfaber2011 <alexfaber2011@gmail.com>
2014-08-23 12:25:24 -0700

    Add yet more info to meal info modal

--- client/views/OfferedMeals/createOfferedMeal.js
+++ client/views/OfferedMeals/createOfferedMeal.js
@@ -12,7 +12,7 @@ Template.createOfferedMeal.events({
 		event.preventDefault();
 
         //#TODO: convert this to autoform; add validation
-		var address = $(event.target).find('[id=streetAddress]').val();
+		var address = $(event.target).find('#streetAddress').val();
 
 		HTTP.get('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCbB-_YPraSIeF3_J4dcKahG77KLdY5G64&address=' +
 			address, function(error, geocoded) {
@@ -35,14 +35,19 @@ Template.createOfferedMeal.events({
                         console.log(geocoded);
                         var lat = accounting.toFixed(geocoded.data.results[0].geometry.location.lat, 10);
                         var lng = accounting.toFixed(geocoded.data.results[0].geometry.location.lng, 10);
+                        var user = Meteor.user();
+                        var userName = user.username;
                         var food = {
-                            name: $(event.target).find('[id=foodName]').val(),
+                            name: $(event.target).find('#foodName').val(),
                             lat: lat,
                             lng: lng,
+                            address: address,
+                            userId: Meteor.userId(),
+                            userName: userName,
                             description: $(event.target).find("#description").val(),
                             numOfServings: $(event.target).find("#numOfServings").val()
                         };
-                        console.log(food);
+//                        console.log(food);
                         OfferedMeals.insert(food);
                     }
 				}
